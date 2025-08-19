a5fb6fd9159e26d2d3f05663532bd93e27f0ee87
Kartik12-bit <kartikeya.shankhdhar_cs24@gla.ac.in>
2025-08-19 15:00:42 +0530

    addition of codes to assisted problem

---
+++ Assisted_Problems/Celsius_Fahrenheit.java
@@ -0,0 +1,6 @@
+public static void main(string[] args){
+        double celsius = 25.0; // Example Celsius value
+        double fahrenheit = (celsius * 9/5) + 32;
+        System.out.println(celsius + " degrees Celsius is equal to " + fahrenheit + " degrees Fahrenheit.");
+    }
+}
