0908c344d1964e4150489584eee11aa6dfd4a696
Kartik12-bit <kartikeya.shankhdhar_cs24@gla.ac.in>
2025-09-14 23:11:42 +0530

    Adding constructor and instance operator questions

---
+++ JavaConstructor/Level2/Vehicle.java
@@ -0,0 +1,42 @@
+/*Create a Vehicle class to manage the details of vehicles:
+Instance Variables: ownerName, vehicleType.
+Class Variable: registrationFee (fixed for all vehicles).
+Methods:
+An instance method displayVehicleDetails() to display owner and vehicle details.
+A class method updateRegistrationFee() to change the registration fee */
+
+class Vehicle {
+    private String ownerName;
+    private String vehicleType;
+    private static double registrationFee = 5000;
+
+    public Vehicle(String ownerName, String vehicleType) {
+        this.ownerName = ownerName;
+        this.vehicleType = vehicleType;
+    }
+
+    public void displayVehicleDetails() {
+        System.out.println("Owner: " + ownerName);
+        System.out.println("Vehicle Type: " + vehicleType);
+        System.out.println("Registration Fee: ₹" + registrationFee);
+        System.out.println("-----------------------------");
+    }
+
+    public static void updateRegistrationFee(double newFee) {
+        registrationFee = newFee;
+        System.out.println("Registration fee updated to: ₹" + registrationFee);
+    }
+
+    public static void main(String[] args) {
+        Vehicle v1 = new Vehicle("Kartik", "Car");
+        Vehicle v2 = new Vehicle("Aman", "Bike");
+
+        v1.displayVehicleDetails();
+        v2.displayVehicleDetails();
+
+        Vehicle.updateRegistrationFee(7000);
+
+        v1.displayVehicleDetails();
+        v2.displayVehicleDetails();
+    }
+}
